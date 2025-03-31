1e4a86bb01f21873c64ed6b1aab82e57a42beb51
markgithub29 <127247960+markgithub29@users.noreply.github.com>
2025-04-01 00:51:01 +0530

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,33 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+}
+
+input {
+    padding: 10px;
+    margin: 5px;
+    width: 80%;
+}
+
+button {
+    padding: 10px;
+    margin: 5px;
+    cursor: pointer;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+}
+
+@media (max-width: 600px) {
+    input, button {
+        width: 100%;
+    }
+}
